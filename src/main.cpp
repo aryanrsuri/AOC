06da36770b206040ee9772a290b129ffa5f467b3
Aryan Suri <arysuri@proton.me>
2025-05-17 19:03:18 -0700

    init
    Signed-off-by: Aryan Suri <arysuri@proton.me>

---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+/**
+ * author: Aryan Suri
+ * licence: MIT
+ **/
+#include <iostream>
+using namespace std;
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int T = 1000;
+  while (T--) {
+    int a, b;
+    cin >> a >> b;
+    cout << a + b << '\n';
+  }
+
+  return 0;
+}
