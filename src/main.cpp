a64d1954d7a6052d423d4dc19788ea5991a55688
Aryan Suri <arysuri@proton.me>
2025-05-17 21:06:27 -0700

    s
    Signed-off-by: Aryan Suri <arysuri@proton.me>

--- src/main.cpp
+++ src/main.cpp
@@ -4,18 +4,27 @@
  **/
 #include <iostream>
 using namespace std;
+const int T = 1000;
 
 int p1() { return 0; }
-
 int p2() { return 0; }
+
 int main() {
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
-  int T = 1000;
-  while (T--) {
-    int a, b;
-    cin >> a >> b;
-    cout << a + b << '\n';
+  int t = T;
+  while (t--) {
+    vector<int> a;
+    while (true) {
+      int x;
+      cin >> x;
+      a.push_back(x);
+      char c;
+      cin >> c;
+      if (c == '\n') {
+        break;
+      }
+    }
   }
 
   return 0;
